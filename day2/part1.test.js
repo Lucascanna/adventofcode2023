e947b225edb246aeb0b0bde3158b45f7fbee3ae6
luca.scannapieco <luca.scannapieco@mia-platform.eu>
2023-12-27 14:50:08 +0100

    fix

--- day2/part1.test.js
+++ day2/part1.test.js
@@ -1,5 +1,5 @@
 const tap = require('tap')
-const { computeNumberOfPossibleGames, computeFewestNumberOfCubes, solve } = require('./part2')
+const { computeNumberOfPossibleGames, solve } = require('./part1')
 
 tap.test('should return the number of possible games', mainTest => {
   mainTest.test('possible game', t => {
@@ -33,7 +33,7 @@ Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`
   const actual = solve(input)
-  const expected = 2286
+  const expected = 8
   mainTest.strictSame(actual, expected)
   mainTest.end()
 })
